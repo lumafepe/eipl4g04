b8e4174329371cffc1e77a30f21ae1d5ff90b561
luis manuel fernandes pereira <lumafepe@gmail.com>
2021-03-23 23:18:41 +0000

    adicionada funçao push responsavel por adicional elementos a stack

--- guiao1.c
+++ guiao1.c
@@ -11,8 +11,15 @@ void inicializador(struct Stack* stack){
     stack->array = (int *) malloc(0 * sizeof(int));//define a array da stack como tendo 0 bits
     stack->comprimento=0;//proxima pos a guardar passa a 0
 }
+// aumenta o tamanho da array e adiciona o elemento no inicio da array
+void push(struct Stack* stack,int eaa){ // eaa --> elemento a adicionar
+    stack->array = realloc(stack->array,(stack->comprimento+1) * sizeof(int)); // aumenta o tamanho da array
+    stack->array[stack->comprimento]=eaa; // guarda o novo valor na array
+    stack->comprimento++; //aumenta a pos do topo
+}
 int main() {
     struct Stack stack; //cria uma stack chamada stack
+    inicializador(&stack);//adiciona valores iniciais a stack
     for (int i=0; i<stack.comprimento; i++){//mostra o conteudo da stack
         printf("%d ", stack.array[i]);
     }
