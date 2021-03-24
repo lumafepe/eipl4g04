176bc9513af1c0d2263c9afe2b3d57efc3686995
luis manuel fernandes pereira <lumafepe@gmail.com>
2021-03-24 18:01:02 +0000

    resuloçao de alguns erros e melhoramento de funçoes

--- guiao1.c
+++ guiao1.c
@@ -2,33 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-//definir o data type (nº de elementos e os elementos)
+//defenir o data type (nº de elementos e os elementos)
 struct Stack{
     int comprimento; //pos do topo
     int *array; // onde os valores sao guardada
 };
-
-// para dar valores iniciais a stack
-void inicializador(struct Stack* stack){
-    stack->array = (int *) malloc(0 * sizeof(int));//define a array da stack como tendo 0 bits
-    stack->comprimento=0;//proxima pos a guardar passa a 0
-}
-
 // aumenta o tamanho da array e adiciona o elemento no inicio da array
 void push(struct Stack* stack,int eaa){ // eaa --> elemento a adicionar
+    stack->comprimento++; //aumenta a pos do topo
     stack->array = realloc(stack->array,(stack->comprimento+1) * sizeof(int)); // aumenta o tamanho da array
     stack->array[stack->comprimento]=eaa; // guarda o novo valor na array
-    stack->comprimento++; //aumenta a pos do topo
 }
 
 // diminui o tamanho da array e devolve o que esta no topo
 int pop(struct Stack* stack){
-    if (stack->comprimento>0){//verificar que tem elementos
-        stack->comprimento--; // diminur posição do topo da lista
+    if (stack->comprimento>=0){//verificar que tem elementos
         int a = stack->array[stack->comprimento]; // valor a devolver
         stack->array = realloc(stack->array,(stack->comprimento) * sizeof(int)); //reduzir o tamanho da array da stack
+        stack->comprimento--; // diminur posição do topo da lista
         return a;
-    }
+    } else return 0;
+}
+
+// para dar valores iniciais a stack
+void inicializador(struct Stack* stack){
+    stack->array = (int *) malloc(0 * sizeof(int));//define a array da stack como tendo 0 bits
+    stack->comprimento=-1;//proxima pos a guardar passa a 0
 }
 
 // equivalente a ++ de c
@@ -38,7 +37,7 @@ void maismais(struct Stack* stack){
 
 // equivalente a -- de c
 void menosmenos(struct Stack* stack){
-    push(stack,pop(stack-1));
+    push(stack,pop(stack)-1);
 }
 
 // equivalente à soma de dois valores
@@ -105,21 +104,20 @@ void leitura2(struct Stack* stack,char c[])
 //faz o tratamento de dados do input do utilizador
 void leitura (struct Stack* stack){
     char str[100];
-    scanf("%[^\n]",&str);//lê a linha inserida pelo utilizador e guarda-a em str
+    assert(scanf("%100[^\n]",str)==1);//lê a linha inserida pelo utilizador e guarda-a em str
     char* token;
     char* rest = str; 
     while ((token = strtok_r(rest, " ", &rest))){ // separa a string por espaçoes e manda para a auxiliar leitura2 cada um dos numeros
         leitura2(stack,token);
     }
-
 }
 
 int main() {
     struct Stack stack; //cria uma stack chamada stack
-    inicializador(&stack);//adiciona valores iniciais a stack
+    inicializador(&stack);//adicona valores iniciais a stack
     leitura(&stack);//trata da leitura e processamento dos inputs do user
-    for (int i=0; i<stack.comprimento; i++){//mostra o conteudo da stack
-        printf("%d ", stack.array[i]);
+    for (int i=0; i<=stack.comprimento; i++){//mostra o conteudo da stack
+        printf("%d", stack.array[i]);
     }
     printf("\n");
     return 0;
